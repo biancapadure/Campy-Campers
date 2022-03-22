b0a477a3a6773772859407dbbe6cd7c24b9a8a09
JasiuPodpora <73708487+JasiuPodpora@users.noreply.github.com>
2022-03-22 18:49:29 +0000

    Update contactUs.php
    Added footer for page

--- version 3/contactUs.php
+++ version 3/contactUs.php
@@ -28,5 +28,33 @@
     </center>
   
 </div>
+	
+	<footer>
+        <div class="footer">
+            <div class="inner_footer">
+                <div class="logo_container">
+                    <img src="Images/logo 3.png">
+
+                </div>
+                <div class="footer_third">
+                    <h1>Need Help?</h1>
+                    <a href="#">Terms &amp; Conditions</a>
+                    <a href="#">Privacy Policy</a>
+                </div>
+
+                <div class="footer_third">
+                    <h1>More</h1>
+                    <a href="#">About Us</a>
+                    <a href="#">Other Sites</a>
+                </div>
+
+                <div class="footer_third">
+                    <h1></h1>
+                    <a href="#">CampyCampers©</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+	
 </body>
 </html>
