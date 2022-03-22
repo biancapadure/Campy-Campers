c50abba90315a718e874b39adb37d6e6c485b131
JasiuPodpora <73708487+JasiuPodpora@users.noreply.github.com>
2022-03-22 18:47:58 +0000

    Update aboutUs.php
    Added footer to page

--- version 3/aboutUs.php
+++ version 3/aboutUs.php
@@ -33,5 +33,32 @@
 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 	
 	</div>
+	
+	<footer>
+        <div class="footer">
+            <div class="inner_footer">
+                <div class="logo_container">
+                    <img src="Images/logo 3.png">
+
+                </div>
+                <div class="footer_third">
+                    <h1>Need Help?</h1>
+                    <a href="#">Terms &amp; Conditions</a>
+                    <a href="#">Privacy Policy</a>
+                </div>
+
+                <div class="footer_third">
+                    <h1>More</h1>
+                    <a href="#">About Us</a>
+                    <a href="#">Other Sites</a>
+                </div>
+
+                <div class="footer_third">
+                    <h1></h1>
+                    <a href="#">CampyCampers©</a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
