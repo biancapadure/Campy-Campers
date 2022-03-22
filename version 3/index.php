b6bf4fbac73e966656019e9f637f189e6eac3d99
JasiuPodpora <73708487+JasiuPodpora@users.noreply.github.com>
2022-03-22 18:51:33 +0000

    Update index.php
    Added footer to page

--- version 3/index.php
+++ version 3/index.php
@@ -29,6 +29,33 @@
     <button><a href="register.php">Register</a></button>
     
 </div>
+    
+    <footer>
+        <div class="footer">
+            <div class="inner_footer">
+                <div class="logo_container">
+                    <img src="Images/logo 3.png">
+
+                </div>
+                <div class="footer_third">
+                    <h1>Need Help?</h1>
+                    <a href="#">Terms &amp; Conditions</a>
+                    <a href="#">Privacy Policy</a>
+                </div>
+
+                <div class="footer_third">
+                    <h1>More</h1>
+                    <a href="#">About Us</a>
+                    <a href="#">Other Sites</a>
+                </div>
+
+                <div class="footer_third">
+                    <h1></h1>
+                    <a href="#">CampyCampers©</a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
