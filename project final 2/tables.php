65a7d25ef151094a783111845d07d85ad27c3122
JasiuPodpora <73708487+JasiuPodpora@users.noreply.github.com>
2022-04-14 23:19:52 +0100

    Update tables.php
    Added fix to error on image line

--- project final 2/tables.php
+++ project final 2/tables.php
@@ -61,7 +61,8 @@
         echo '<tr>'; // html table row tag
         echo '<td>'.$Row['stockDescription'].'</td>'; // html Table detail tags
         echo '<td>'.$Row['Price'].'</td>';
-        echo '<td>'.'<img src="'.$Row['imglink'].'" alt="image of table" />'.'</td>';
+        $imglink=$Row['imgLink'];
+        echo '<td>'.'<img id="image1" src="'.$imglink.'"alt="image of table"/>'.'</td>';
         echo '<td>'.'<input type = "submit" name="your_button_name" value = "Add to Basket">'.'</td>';
         echo '</tr>';
         }
